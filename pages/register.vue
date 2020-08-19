37e3c181b729a92853d89ec9ffe280dd9e7850cc
sunxiaojie79 <297280911@qq.com>
2020-08-19 22:53:57 +0800

    feat:注册页面

--- pages/register.vue
+++ pages/register.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container">
-		<el-form class="login-form" label-width="100px" :model="form" :rules="rules" red="registerForm">
+		<el-form class="login-form" label-width="100px" :model="form" :rules="rules" ref="registerForm">
 			<el-form-item prop="email" label="邮箱">
 				<el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
 			</el-form-item>
@@ -34,43 +34,76 @@
 </template>
 
 <script type="text/ecmascript-6">
+import md5 from 'md5';
 export default {
-    layout: 'login',
-    name: "",
-    data() {
-      return {
-				form: {
-					email: '',
-					nickname: '',
-					passwd: '',
-					repasswd: '',
-					captcha: '',
-				},
-				rules:{
-					email: [
-						{ required: true, message: '请输入邮箱'},
-						{ type: 'email', message: '请输入正确的邮箱格式'},
-					],
-					captcha: [
-						{ required: true, message: '请输入验证码'},
-					],
-					nickname: [
-						{ required: true, message: '请输入昵称'},
-					],
-					passwd: [
-						{ required: true, message: '请输入密码'},
-					],
-					repasswd: [
-						{ required: true, message: '请再次输入密码'},
-					],
-				},
-				code: {
-					captcha: '/api/captcha'
-				},
-			}
-    },
-    components: {},
-    methods: {
+	layout: 'login',
+	name: "",
+	data() {
+		return {
+			form: {
+				email: '',
+				nickname: '',
+				passwd: '',
+				repasswd: '',
+				captcha: '',
+			},
+			rules:{
+				email: [
+					{ required: true, message: '请输入邮箱'},
+					{ type: 'email', message: '请输入正确的邮箱格式'},
+				],
+				captcha: [
+					{ required: true, message: '请输入验证码'},
+				],
+				nickname: [
+					{ required: true, message: '请输入昵称'},
+				],
+				passwd: [
+					{ required: true, pattern: /^[\w_-]{6,12}$/g, message: '请输入6~12位密码'},
+				],
+				repasswd: [
+					{ required: true, message: '请再次输入密码'},
+					{ validator: (rule, value, cb) => {
+						if(value !== this.form.passwd){
+							cb('两次密码不一致');
+						}
+						cb()
+					}}
+				],
+			},
+			code: {
+				captcha: '/api/captcha'
+			},
+		}
+	},
+	components: {},
+	methods: {
+		handleRegister(){
+			this.$refs.registerForm.validate(async valid => {
+				if(valid){
+					const { email, nickname, passwd, captcha } = this.form;
+					console.log('校验成功');
+					const params = {
+						email,
+						nickname,
+						passwd: md5(passwd),
+						captcha,
+					}
+					let res = await this.$http.post('/user/register', params);
+					console.log('res', res)
+					if(res.code === 0){
+						this.$alert('注册成功', '成功', {
+							confirmButtonText:"去登录",
+              callback:()=>{
+                this.$router.push("/login")
+              }
+						})
+					}else{
+            this.$message.error(res.message)
+          }
+				}
+			})
+		},
 		resetCaptcha(){
 			this.code.captcha = '/api/captcha?_t=' + new Date().getTime()
 		}
